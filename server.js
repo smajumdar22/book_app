8a518bf43060680b479b95e1e48e6bd2ed8bba08
iptmh <tammyip@gmail.com>
2019-11-20 21:30:50 -0800

    working codes

--- server.js
+++ server.js
@@ -9,32 +9,28 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', newSearch);
-app.post('/searches' , createSearch);
+app.post('/searches', createSearch);
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
-function newSearch(req,res){
+function newSearch(req, res) {
   res.render('pages/index');
 }
 
-function createSearch(req,res){
+function createSearch(req, res) {
   console.log(req.body.search);
   let url = 'https://www.googleapis.com/books/v1/volumes?q=';
-  if(req.body.search[1] === 'title') {url += `intitle:${req.body.search[0]}`;}
-  if(req.body.search[1] === 'author') {url += `inauthor:${req.body.search[0]}`;}
+  if (req.body.search[1] === 'title') { url += `intitle:${req.body.search[0]}`; }
+  if (req.body.search[1] === 'author') { url += `inauthor:${req.body.search[0]}`; }
 
   superagent.get(url)
-    .then(apiResponse =>{
-      apiResponse.body.items.map(bookResults => {
-        new Book(bookResults.volumeInfo);
-      });
-    })
-    .then(results => res.render('pages/searches', {searchResults: results}));
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => res.render('pages/searches/show.ejs', { searchResults: results }));
 }
-function Book(info){
+function Book(info) {
   console.log(info.title);
   this.title = info.title || 'No title available';
 }
