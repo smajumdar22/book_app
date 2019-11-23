d2fa35b2d32c3ffccf35d4f55c66bc64d705de17
Shubham Majumdar <smajumdar22@gmail.com>
2019-11-23 14:27:44 -0800

    Add server changes

--- book.sql
+++ book.sql
@@ -1,15 +1,15 @@
-DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS books;
 
-CREATE TABLE Books (
+CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     authors VARCHAR(80),
     title text,
     isbn varchar(30),
     image_url text,
-    description text,
-    bookshelf int
+    description_book text,
+    bookshelf text
 );
 
-INSERT INTO Books (id, authors,title,isbn,image_url,description,bookshelf)
-VALUES ('Roland Mushat Frye', 'Shakespeare','ISBN_10 0195160932','http://books.google.com/books/content?id=XMrZrA1vomQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api','Looks at the life, career, works, and influence of William Shakespeare.'),
-('Stanley W. Wells', 'Shakespeare','ISBN_10 0415352894','http://books.google.com/books/content?id=sE_vuDVQwNkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api','Introduction; Part I: Theory And Practice; Part II: Culture And Tradition; Part III: Text And Ideology; Part IV: Stage And Spectacle; Afterword; Select bibliography; Index.')
+INSERT INTO books (id, authors,title,isbn,image_url,description_book)
+VALUES (1,'Roland Mushat Frye', 'Shakespeare','ISBN_10 0195160932','http://books.google.com/books/content?id=XMrZrA1vomQC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api','Looks at the life, career, works, and influence of William Shakespeare.'),
+(2,'Stanley W. Wells', 'Shakespeare','ISBN_10 0415352894','http://books.google.com/books/content?id=sE_vuDVQwNkC&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api','Introduction; Part I: Theory And Practice; Part II: Culture And Tradition; Part III: Text And Ideology; Part IV: Stage And Spectacle; Afterword; Select bibliography; Index.');
